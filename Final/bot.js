4c1e30b1a0466ea81b770cd12ff58fbb08eed839
Meme-sama <56776697+Meme-sama@users.noreply.github.com>
2019-12-14 21:47:48 -0500

    added tern. (-dev) finished playMusic, 'play', and 'stop'.

--- Final/bot.js
+++ Final/bot.js
@@ -2,38 +2,76 @@ const Discord = require('discord.js');
 const botsuki = new Discord.Client();
 const {token, prefix} = require('./auth.json');
 const ytdl = require('ytdl-core');
+const serverQ = {}; // used to store the Q (queue) of servers that request music
+
+let thisServer; // specifies which server is actively looking for directions
+
 botsuki.on('ready', () => {
     console.log(`${botsuki.user.tag} has awoken!`);
 });
 
-const serverQ = [];
 
 botsuki.login(token);
 
+function playMusic(connection, msg) {
+  /*
+  Adds url to Q then
+  plays current url and then removes it from Q.
+
+  Check if another song is in Q,
+  if not, leave VC
+  */
+  thisServer = serverQ[msg.guild.id];
+  const stream = ytdl(thisServer.q[0], {filter: ('audioonly')});
+  console.log(stream);
+  thisServer.dispatcher = connection.playStream(stream, {volume : 100});
+  thisServer.q.shift();
+  thisServer.dispatcher.on('end', () => {
+    if (thisServer[0]) {
+      playMusic(connection, msg);
+    }
+    else connection.diconnect();
+    });
+}
+
 botsuki.on('message', msg=> {
   if (msg.content.substr(0, 1) === prefix) {
-    const arg = msg.content.substr(1).split(' ');
-    switch (arg[0]) {
+    const cmd = msg.content.substr(1).split(' ');
+    switch (cmd[0]) {
       case 'ping':
-        msg.reply('pong!');
+        msg.reply('pang!');
         break;
       case 'play':
-        if (!arg[1]) {
-          msg.channel.send('Umm... a link, please!'); return;
+        if (!cmd[1]) {
+          msg.channel.send('Umm... a link, Please!'); return;
         }
         if (!msg.member.voiceChannel) {
           msg.channel.send('Join a voice channel, Please!'); return;
         }
         if (!serverQ[msg.guild.id]) {
-          const a = 2;
+          serverQ[msg.guild.id] = { // creates property 'msg.guild.id'
+            q: [],
+          };
         }
-        else {
-          msg.member.voiceChannel.Join()
-          .then(()=> {
-            msg.channel.send("Let's talk, Please!");
-          });
-        }
+        msg.member.voiceChannel.join()
+        .then(connection=> {
+            msg.channel.send('Let\'s talk, Please!');
+            serverQ[msg.guild.id].q.push(cmd[1]);
+            playMusic(connection, msg);
+        }).catch(console.err);
         break;
+      case 'clearQ':
+        serverQ[msg.guild.id] = false;
+        console.log(serverQ);
+        break;
+      case 'stop':
+        if (msg.voiceConnection) {
+          for (var i = 0; i < thisServer.q.length - 1; i++) {
+            thisServer.q.splice(i, 1);
+          }
+          thisServer.dispatcher.end();
+          console.log('bye!').catch(err => console.log(err));
+        }
     }
   }
 });
